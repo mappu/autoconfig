fc1e3e21aac97e6e196fed4da2f53761b4f763b4
mappu <mappu04@gmail.com>
2025-11-14 12:23:06 +1300

    autoconfig: extract types to registry

--- config.go
+++ config.go
@@ -1,9 +1,7 @@
 package autoconfig
 
 import (
-	"path/filepath"
 	"reflect"
-	"strconv"
 	"strings"
 
 	qt "github.com/mappu/miqt/qt6"
@@ -77,155 +75,14 @@ func makeConfigAreaFor(obj reflect.Type, area *qt.QFormLayout) func() Configurab
 			widgetType = "__childStruct"
 		}
 
-		switch widgetType {
-		case "bool":
-			rbtn := qt.NewQCheckBox3(label)
-			area.AddRow3("", rbtn.QWidget)
-			onApply = append(onApply, makeAssigner(i, func(rv *reflect.Value) {
-				rv.SetBool(rbtn.IsChecked())
-			}))
-
-		case "string", "Password":
-			rline := qt.NewQLineEdit2()
-			if widgetType == "Password" {
-				rline.SetEchoMode(qt.QLineEdit__Password)
-
-			}
-			if useInit, ok := ff.Tag.Lookup("yinit"); ok {
-				rline.SetText(useInit)
-			}
-			area.AddRow3(label+`:`, rline.QWidget)
-			onApply = append(onApply, makeAssigner(i, func(rv *reflect.Value) {
-				rv.SetString(rline.Text())
-			}))
-
-		case "EnumList":
-			enumOpts, _ := ff.Tag.Lookup("yenum")
-
-			rcombo := qt.NewQComboBox2()
-			rcombo.AddItems(strings.Split(enumOpts, `;;`)) // Same separator as Qt filter (yfilter)
-
-			onApply = append(onApply, makeAssigner(i, func(rv *reflect.Value) {
-				rv.SetInt(int64(rcombo.CurrentIndex()))
-			}))
-
-			area.AddRow3(label+`:`, rcombo.QWidget)
-
-		case "ExistingFile", "ExistingDirectory":
-			hbox := qt.NewQHBoxLayout2()
-			hbox.SetContentsMargins(0, 0, 0, 0)
-
-			rline := qt.NewQLineEdit2()
-			hbox.AddWidget(rline.QWidget)
-
-			if useInit, ok := ff.Tag.Lookup("yinit"); ok {
-				rline.SetText(useInit)
-			}
-
-			browseBtn := qt.NewQPushButton3("Browse...")
-			hbox.AddWidget(browseBtn.QWidget)
-
-			filter := "All files (*.*)"
-			if useFilter, ok := ff.Tag.Lookup("yfilter"); ok {
-				filter = useFilter
-			}
-
-			if widgetType == "ExistingDirectory" {
-				browseBtn.OnClicked(func() {
-					openDir := qt.QFileDialog_GetExistingDirectory3(browseBtn.QWidget, "Select a database directory...", rline.Text())
-					if openDir != "" {
-						rline.SetText(openDir)
-					}
-				})
-
-			} else if widgetType == "ExistingFile" {
-				browseBtn.OnClicked(func() {
-					startDir := filepath.Dir(rline.Text())
-
-					openPath := qt.QFileDialog_GetOpenFileName4(browseBtn.QWidget, "Select a database file...", startDir, filter)
-					if openPath != "" {
-						rline.SetText(openPath)
-					}
-				})
-			}
-
-			onApply = append(onApply, makeAssigner(i, func(rv *reflect.Value) {
-				rv.SetString(rline.Text())
-			}))
-
-			hboxWidget := qt.NewQWidget2()
-			hboxWidget.SetLayout(hbox.QLayout)
-			area.AddRow3(label+`:`, hboxWidget)
-
-		case "AddressPort":
-			hbox := qt.NewQHBoxLayout2()
-			hbox.SetContentsMargins(0, 0, 0, 0)
-
-			addr := qt.NewQLineEdit2()
-			hbox.AddWidget(addr.QWidget)
-
-			separator := qt.NewQLabel3(`:`)
-			hbox.AddWidget(separator.QWidget)
-
-			port := qt.NewQSpinBox2()
-			hbox.AddWidget(port.QWidget)
-			port.SetMinimum(0)
-			port.SetMaximum(65535)
-
-			if defaultVal, ok := ff.Tag.Lookup(`yport`); ok {
-				defaultValInt, err := strconv.Atoi(defaultVal)
-				if err != nil {
-					panic(err)
-				}
-				port.SetValue(defaultValInt)
-			}
-
-			onApply = append(onApply, makeAssigner(i, func(rv *reflect.Value) {
-				newVal := AddressPort{Address: addr.Text(), Port: port.Value()}
-				rv.Set(reflect.ValueOf(newVal))
-			}))
-
-			hboxWidget := qt.NewQWidget2()
-			hboxWidget.SetLayout(hbox.QLayout)
-			area.AddRow3(label+`:`, hboxWidget)
-
-		case "__childStruct":
-			hbox := qt.NewQHBoxLayout2()
-			hbox.SetContentsMargins(0, 0, 0, 0)
-
-			statusField := qt.NewQLabel2()
-			statusField.SetText("Not configured")
-			statusField.SetSizePolicy2(qt.QSizePolicy__Expanding, qt.QSizePolicy__Maximum)
-			hbox.AddWidget(statusField.QWidget)
-
-			configBtn := qt.NewQToolButton2()
-			configBtn.SetText("Edit...")
-			configBtn.OnClicked(func() {
-
-				// Allocate a temporary variable with type of the struct.
-				//    v.Elem() is the value contained in the interface.
-
-				openDialogFor(ff.Type.Elem(), configBtn.QWidget, label, func(childThing ConfigurableStruct) {
-					// ...
-				})
-			})
-			hbox.AddWidget(configBtn.QWidget)
-
-			clearBtn := qt.NewQToolButton2()
-			clearBtn.SetText("\u00d7") // &times; ×
-			hbox.AddWidget(clearBtn.QWidget)
-
-			onApply = append(onApply, makeAssigner(i, func(rv *reflect.Value) {
-				// ...
-			}))
-
-			hboxWidget := qt.NewQWidget2()
-			hboxWidget.SetLayout(hbox.QLayout)
-			area.AddRow3(label+`:`, hboxWidget)
-
-		default:
+		handler, ok := registeredTypes[widgetType]
+		if !ok {
 			panic("makeConfigArea missing handling for type=" + widgetType)
 		}
+
+		singleFieldSaver := handler(area, ff.Type, ff.Tag, label)
+
+		onApply = append(onApply, makeAssigner(i, singleFieldSaver))
 
 	}
 
--- types.go
+++ types.go
@@ -1,5 +1,14 @@
 package autoconfig
 
+import (
+	"path/filepath"
+	"reflect"
+	"strconv"
+	"strings"
+
+	qt "github.com/mappu/miqt/qt6"
+)
+
 type ConfigurableStruct interface{}
 
 type AddressPort struct {
@@ -14,3 +23,225 @@ type ExistingDirectory string
 type Password string
 
 type EnumList int
+
+type saveHandler func(rv *reflect.Value)
+
+type typeHandler func(area *qt.QFormLayout, typ reflect.Type, tag reflect.StructTag, label string) saveHandler
+
+var (
+	registeredTypes map[string]typeHandler
+)
+
+func handle_bool(area *qt.QFormLayout, typ reflect.Type, tag reflect.StructTag, label string) saveHandler {
+	rbtn := qt.NewQCheckBox3(label)
+	area.AddRow3("", rbtn.QWidget)
+
+	return func(rv *reflect.Value) {
+		rv.SetBool(rbtn.IsChecked())
+	}
+}
+
+func handle_string(area *qt.QFormLayout, typ reflect.Type, tag reflect.StructTag, label string) saveHandler {
+	rline := qt.NewQLineEdit2()
+	if useInit, ok := tag.Lookup("yinit"); ok {
+		rline.SetText(useInit)
+	}
+	area.AddRow3(label+`:`, rline.QWidget)
+	return func(rv *reflect.Value) {
+		rv.SetString(rline.Text())
+	}
+}
+
+func handle_Password(area *qt.QFormLayout, typ reflect.Type, tag reflect.StructTag, label string) saveHandler {
+	rline := qt.NewQLineEdit2()
+	rline.SetEchoMode(qt.QLineEdit__Password)
+	if useInit, ok := tag.Lookup("yinit"); ok {
+		rline.SetText(useInit)
+	}
+	area.AddRow3(label+`:`, rline.QWidget)
+	return func(rv *reflect.Value) {
+		rv.SetString(rline.Text())
+	}
+}
+
+func handle_EnumList(area *qt.QFormLayout, typ reflect.Type, tag reflect.StructTag, label string) saveHandler {
+	enumOpts, _ := tag.Lookup("yenum")
+
+	rcombo := qt.NewQComboBox2()
+	rcombo.AddItems(strings.Split(enumOpts, `;;`)) // Same separator as Qt filter (yfilter)
+
+	area.AddRow3(label+`:`, rcombo.QWidget)
+
+	return func(rv *reflect.Value) {
+		rv.SetInt(int64(rcombo.CurrentIndex()))
+	}
+}
+
+func handle_ExistingFile(area *qt.QFormLayout, typ reflect.Type, tag reflect.StructTag, label string) saveHandler {
+	hbox := qt.NewQHBoxLayout2()
+	hbox.SetContentsMargins(0, 0, 0, 0)
+
+	rline := qt.NewQLineEdit2()
+	hbox.AddWidget(rline.QWidget)
+
+	if useInit, ok := tag.Lookup("yinit"); ok {
+		rline.SetText(useInit)
+	}
+
+	browseBtn := qt.NewQPushButton3("Browse...") // TODO document-open
+	hbox.AddWidget(browseBtn.QWidget)
+
+	filter := "All files (*.*)"
+	if useFilter, ok := tag.Lookup("yfilter"); ok {
+		filter = useFilter
+	}
+
+	browseBtn.OnClicked(func() {
+		startDir := filepath.Dir(rline.Text())
+
+		openPath := qt.QFileDialog_GetOpenFileName4(browseBtn.QWidget, "Select a database file...", startDir, filter)
+		if openPath != "" {
+			rline.SetText(openPath)
+		}
+	})
+
+	hboxWidget := qt.NewQWidget2()
+	hboxWidget.SetLayout(hbox.QLayout)
+	area.AddRow3(label+`:`, hboxWidget)
+
+	return func(rv *reflect.Value) {
+		rv.SetString(rline.Text())
+	}
+}
+
+func handle_ExistingDirectory(area *qt.QFormLayout, typ reflect.Type, tag reflect.StructTag, label string) saveHandler {
+	hbox := qt.NewQHBoxLayout2()
+	hbox.SetContentsMargins(0, 0, 0, 0)
+
+	rline := qt.NewQLineEdit2()
+	hbox.AddWidget(rline.QWidget)
+
+	if useInit, ok := tag.Lookup("yinit"); ok {
+		rline.SetText(useInit)
+	}
+
+	browseBtn := qt.NewQPushButton3("Browse...") // TODO folder-open
+	hbox.AddWidget(browseBtn.QWidget)
+
+	browseBtn.OnClicked(func() {
+		openDir := qt.QFileDialog_GetExistingDirectory3(browseBtn.QWidget, "Select a database directory...", rline.Text())
+		if openDir != "" {
+			rline.SetText(openDir)
+		}
+	})
+
+	hboxWidget := qt.NewQWidget2()
+	hboxWidget.SetLayout(hbox.QLayout)
+	area.AddRow3(label+`:`, hboxWidget)
+
+	return func(rv *reflect.Value) {
+		rv.SetString(rline.Text())
+	}
+}
+
+func handle_AddressPort(area *qt.QFormLayout, typ reflect.Type, tag reflect.StructTag, label string) saveHandler {
+	hbox := qt.NewQHBoxLayout2()
+	hbox.SetContentsMargins(0, 0, 0, 0)
+
+	addr := qt.NewQLineEdit2()
+	hbox.AddWidget(addr.QWidget)
+
+	separator := qt.NewQLabel3(`:`)
+	hbox.AddWidget(separator.QWidget)
+
+	port := qt.NewQSpinBox2()
+	hbox.AddWidget(port.QWidget)
+	port.SetMinimum(0)
+	port.SetMaximum(65535)
+
+	if defaultVal, ok := tag.Lookup(`yport`); ok {
+		defaultValInt, err := strconv.Atoi(defaultVal)
+		if err != nil {
+			panic(err)
+		}
+		port.SetValue(defaultValInt)
+	}
+
+	hboxWidget := qt.NewQWidget2()
+	hboxWidget.SetLayout(hbox.QLayout)
+	area.AddRow3(label+`:`, hboxWidget)
+
+	return func(rv *reflect.Value) {
+		newVal := AddressPort{Address: addr.Text(), Port: port.Value()}
+		rv.Set(reflect.ValueOf(newVal))
+	}
+}
+
+func handle_ChildStructPtr(area *qt.QFormLayout, typ reflect.Type, tag reflect.StructTag, label string) saveHandler {
+
+	// Allocate a temporary variable with type of the struct.
+	wipValue := reflect.New(typ.Elem()) // struct itself, not pointer
+
+	hbox := qt.NewQHBoxLayout2()
+	hbox.SetContentsMargins(0, 0, 0, 0)
+
+	statusField := qt.NewQLabel2()
+	statusField.SetText("Not configured")
+	statusField.SetSizePolicy2(qt.QSizePolicy__Expanding, qt.QSizePolicy__Maximum)
+	hbox.AddWidget(statusField.QWidget)
+
+	configBtn := qt.NewQToolButton2()
+	if qt.QIcon_HasThemeIcon("edit-symbolic") {
+		configBtn.SetIcon(qt.QIcon_FromTheme("edit-symbolic"))
+		configBtn.SetToolTip("Edit...")
+	} else {
+		configBtn.SetText("Edit...")
+	}
+	configBtn.OnClicked(func() {
+
+		openDialogFor(typ.Elem(), configBtn.QWidget, label, func(childThing ConfigurableStruct) {
+			if childThing == nil {
+				// ???
+			} else {
+				// childThing is interface
+				iface := reflect.ValueOf(childThing)
+				//ptrtostruct := iface.Elem()
+				wipValue.Set(iface.Elem())
+			}
+		})
+	})
+	hbox.AddWidget(configBtn.QWidget)
+
+	clearBtn := qt.NewQToolButton2()
+	if qt.QIcon_HasThemeIcon("edit-clear") {
+		clearBtn.SetIcon(qt.QIcon_FromTheme("edit-clear"))
+	} else {
+		clearBtn.SetText("\u00d7") // &times; ×
+	}
+	clearBtn.SetToolTip("Clear")
+	hbox.AddWidget(clearBtn.QWidget)
+
+	hboxWidget := qt.NewQWidget2()
+	hboxWidget.SetLayout(hbox.QLayout)
+	area.AddRow3(label+`:`, hboxWidget)
+
+	return func(rv *reflect.Value) {
+		// rv should be a pointer-to-struct, but our 'wipValue' is struct itself
+		// Create a new pointer-to-struct value
+		// TODO not sure if this works??
+		// rv.Elem().Set(wipValue)
+	}
+}
+
+func init() {
+	registeredTypes = map[string]typeHandler{
+		"bool":              handle_bool,
+		"string":            handle_string,
+		"Password":          handle_Password,
+		"EnumList":          handle_EnumList,
+		"ExistingFile":      handle_ExistingFile,
+		"ExistingDirectory": handle_ExistingDirectory,
+		"AddressPort":       handle_AddressPort,
+		"__childStruct":     handle_ChildStructPtr,
+	}
+}
